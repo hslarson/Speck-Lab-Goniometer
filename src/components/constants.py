126c86db8ccd2e5f3eecebb2b9da3c0e85cf40f8
Hunter Larson <hslarson@umail.ucsb.edu>
2025-05-31 22:18:07 -0700

    Motors working

---
+++ src/components/constants.py
@@ -0,0 +1,16 @@
+
+# USB Constants
+ZABER_USB_SERIAL_NUM = "A10NGBR4A"
+ZABER_USB_BAUD_RATE = 115200
+
+# Rotary Motor Constants
+ZABER_AZIMUTH_SERIAL_NUM  = 132636
+ZABER_ALTITUDE_SERIAL_NUM = 132641
+
+ZABER_AZIMUTH_ANGLE_OFFSET  = 0 # (degrees) The angle reading when azimuth is 0
+ZABER_ALTITUDE_ANGLE_OFFSET = -45 # (degrees) The angle reading when arm is vertical
+
+# Linear Motor Constants
+ZABER_X_SERIAL_NUM = 137246 # +X = Table left
+ZABER_Y_SERIAL_NUM = 137247 # +Y = Table rear
+ZABER_Z_SERIAL_NUM = 137248 # +Z = Floor
--- src/components/motors.py
+++ src/components/motors.py
@@ -1,17 +1,13 @@
 import serial.tools.list_ports
 from zaber_motion import Units
-from zaber_motion.ascii import Connection, Axis
-
-# Constants
-ZABER_AZIMUTH_SERIAL_NUM  = 132636
-ZABER_ALTITUDE_SERIAL_NUM = 132641
-
-ZABER_AZIMUTH_ANGLE_OFFSET  = 0 # The angle reading when azimuth is 0
-ZABER_ALTITUDE_ANGLE_OFFSET = -45 # The angle reading when arm is vertical
+from zaber_motion.ascii import Connection
+from constants import *
 
 
-class ZaberRotaryStage:
-    def __init__(self, serial_num, angle_offset=0.0, usb_serial_num="A10NGBR4A", baud_rate=115200):
+class ZaberMotor:
+    """High-level class for both linear and rotary stages"""
+
+    def __init__(self, serial_num, usb_serial_num=ZABER_USB_SERIAL_NUM, baud_rate=ZABER_USB_BAUD_RATE):
         """
         Params:
         - serial_num (int): Device serial number
@@ -26,20 +22,16 @@ class ZaberRotaryStage:
 
         # Device info
         self._serial_num = serial_num
-        self._angle_offset = angle_offset # TODO: Can I replace this logic with limit.home.x settings?
 
-        # Device/Axis Objects
+        # Declare Device/Axis objects
         self._device = None
         self._axis = None
 
+        # Connect to device
+        self.connect()
 
-    def connect(self, axis_number=1):
-        """
-        Connect to Zaber Stage via USB
-        
-        Params:
-        - axis_number (int): The axis number to connect to
-        """
+    def connect(self):
+        """Connect to Zaber Stage via USB"""
 
         # Search COM ports for Zaber device
         ports = serial.tools.list_ports.comports()
@@ -47,7 +39,6 @@ class ZaberRotaryStage:
             # Open connection if device is found
             if p.serial_number == self._usb_serial_num:
                 conn = Connection.open_serial_port(p.device, self._baud_rate)
-                print("Serial communication established")
                 break
         else:
             print("Could not find Zaber motor in COM devices")
@@ -58,8 +49,8 @@ class ZaberRotaryStage:
         for device in device_list:
             if device.serial_number == self._serial_num:
                 self._device = device
-                self._axis = device.get_axis(axis_number)
-                print("Found device")
+                self._axis = device.get_axis(1)
+                print(f"Connected to Device (SN={self._serial_num})")
                 break
         else:
             print("Failed to find device with specified serial number")
@@ -67,56 +58,174 @@ class ZaberRotaryStage:
 
 
 
-def init_axes(azimuth_axis: Axis, altitude_axis: Axis):
-    """Initialize settings and home axes"""
+class ZaberRotaryStage(ZaberMotor):
+    """Wrapper for controlling Zaber rotary stage"""
 
-    # Set axis limits
-    azimuth_axis.settings.set("limit.min", -90-ZABER_AZIMUTH_ANGLE_OFFSET, Units.ANGLE_DEGREES)
-    azimuth_axis.settings.set("limit.max",  90-ZABER_AZIMUTH_ANGLE_OFFSET, Units.ANGLE_DEGREES)
+    def __init__(self, serial_num, usb_serial_num=ZABER_USB_SERIAL_NUM, baud_rate=ZABER_USB_BAUD_RATE):
+        """
+        Params:
+        - serial_num (int): Device serial number
+        - usb_serial_num (str): Serial number of USB device controller
+        - baud_rate (int): Serial communication baud rate
+        """
 
-    altitude_axis.settings.set("limit.min", -90-ZABER_ALTITUDE_ANGLE_OFFSET, Units.ANGLE_DEGREES)
-    altitude_axis.settings.set("limit.max",  90-ZABER_ALTITUDE_ANGLE_OFFSET, Units.ANGLE_DEGREES)
+        # Initialize parent class
+        super().__init__(
+            serial_num=serial_num,
+            usb_serial_num=usb_serial_num,
+            baud_rate=baud_rate
+        )
 
-    # Safely home stages
-    # If we don't ensure that the angle is positive,
-    # The motor may rotate in the wrong direction while homing
-    print("Homing stages")
-    az_pos = azimuth_axis.get_position(Units.ANGLE_DEGREES)
-    assert abs(az_pos) < 180, "Azimuth position is large. Please manually move closer to zero."
-    if az_pos < 0: azimuth_axis.move_relative(-az_pos+5, Units.ANGLE_DEGREES)
-    azimuth_axis.home()
+        # Angular offset
+        self._angle_offset = 0.0 # TODO: Can I replace this logic with limit.home.x settings?
 
-    alt_pos = altitude_axis.get_position(Units.ANGLE_DEGREES)
-    assert abs(alt_pos) < 180, "Altitude position is large. Please manually move closer to zero."
-    if alt_pos < 0: altitude_axis.move_relative(-alt_pos+5, Units.ANGLE_DEGREES)
-    altitude_axis.home()
+    def configure(self, angle_offset=None, limit_min=None, limit_max=None, max_speed=None, max_accel=None):
+        """
+        Configure the rotary stage
+        Parameters:
+        - angle_offset (float): Encoder reading (degrees) when axis is at "0" position
+        - limit_min (float): Minimum allowable angle in degrees
+        - limit_max (float): Maximum allowable angle in degrees
+        - max_speed (float): Maximum rotational speed in degrees/second
+        - max_accel (float): Maximum rotational acceleration in degrees/second^2
+        """
+        # Set angle offset
+        if angle_offset:
+            self._angle_offset = angle_offset
+
+        # Set axis limits
+        if limit_min:
+            self._axis.settings.set("limit.min", limit_min-self._angle_offset, Units.ANGLE_DEGREES)
+        if limit_max:
+            self._axis.settings.set("limit.max",  limit_max-self._angle_offset, Units.ANGLE_DEGREES)
+
+        # Set max speed/accel
+        if max_speed:
+            self._axis.settings.set('maxspeed', max_speed, Units.ANGULAR_VELOCITY_DEGREES_PER_SECOND)
+        if max_accel:
+            self._axis.settings.set('accel', max_accel, Units.ANGULAR_ACCELERATION_DEGREES_PER_SECOND_SQUARED)
+
+    def set_angle(self, angle_deg):
+        self._axis.move_absolute(angle_deg-self._angle_offset, Units.ANGLE_DEGREES)
+
+    def move_by(self, offset_deg):
+        """Move relative amount in degrees"""
+        self._axis.move_relative(offset_deg, Units.ANGLE_DEGREES)
+
+    def get_angle(self):
+        return self._axis.get_position(Units.ANGLE_DEGREES)
+
+    def home(self):
+        """Safely home axis. Make sure the axis moves in the correct direction."""
+        pos = self.get_angle()
+        assert abs(pos) < 180, "Angular position is too large. Please manually move closer to zero."
+        if pos < 0: self._axis.move_relative(-pos+5, Units.ANGLE_DEGREES)
+        self._axis.home()
 
 
-def set_altitude(axis: Axis, angle_deg):
-    """
-    Set the position of the altitude stage:\n
-      0 = Vertical\n
-    -90 = Left\n
-    +90 = Right
-    """
-    axis.move_absolute(angle_deg-ZABER_ALTITUDE_ANGLE_OFFSET, Units.ANGLE_DEGREES)
 
+class ZaberLinearStage(ZaberMotor):
+    """Wrapper for controlling Zaber linear stage"""
 
-def set_azimuth(axis: Axis, angle_deg):
-    """
-    Set the position of the azimuth stage:\n
-      0 = Probes in front\n
-    -90 = Probes on right\n
-    +90 = Probes on left
-    """
-    axis.move_absolute(angle_deg-ZABER_AZIMUTH_ANGLE_OFFSET, Units.ANGLE_DEGREES)
+    def configure(self, limit_min=None, limit_max=None, max_speed=None, max_accel=None):
+        """
+        Configure the rotary stage
+        Parameters:
+        - limit_min (float): Minimum allowable position in millimeters
+        - limit_max (float): Maximum allowable angle in millimeters
+        - max_speed (float): Maximum rotational speed in millimeters/second
+        - max_accel (float): Maximum rotational acceleration in millimeters/second^2
+        """
+        # Set axis limits
+        if limit_min:
+            self._axis.settings.set("limit.min", limit_min, Units.LENGTH_MILLIMETRES)
+        if limit_max:
+            self._axis.settings.set("limit.max", limit_max, Units.LENGTH_MILLIMETRES)
+
+        # Set max speed/accel
+        if max_speed:
+            self._axis.settings.set('maxspeed', max_speed, Units.VELOCITY_MILLIMETRES_PER_SECOND)
+        if max_accel:
+            self._axis.settings.set('accel', max_accel, Units.ACCELERATION_MILLIMETRES_PER_SECOND_SQUARED)
+
+    def set_position(self, position_mm):
+        """Set absolute position in mm"""
+        self._axis.move_absolute(position_mm, Units.LENGTH_MILLIMETRES)
+
+    def move_by(self, offset_mm):
+        """Move relative amount in mm"""
+        self._axis.move_relative(offset_mm, Units.LENGTH_MILLIMETRES)
+
+    def get_angle(self):
+        """Get absolute position in mm"""
+        return self._axis.get_position(Units.LENGTH_MILLIMETRES)
+    
+    def home(self):
+        """Home axis"""
+        self._axis.home()
+
 
 
 if __name__ == "__main__":
-    az, alt = get_axes()
-    init_axes(az, alt)
+    
+    # Connect to rotary stages
+    azimuth_stage  = ZaberRotaryStage(ZABER_AZIMUTH_SERIAL_NUM)
+    altitude_stage = ZaberRotaryStage(ZABER_ALTITUDE_SERIAL_NUM)
 
-    print("starting move")
-    set_altitude(alt, 0)
-    print("done")
-    set_azimuth(az, 45)
+    # Connect to linear stages
+    x_stage = ZaberLinearStage(ZABER_X_SERIAL_NUM)
+    y_stage = ZaberLinearStage(ZABER_Y_SERIAL_NUM)
+    z_stage = ZaberLinearStage(ZABER_Z_SERIAL_NUM)
+
+    # Home stages
+    azimuth_stage.home()
+    altitude_stage.home()
+    x_stage.home()
+    y_stage.home()
+    z_stage.home()
+
+    # Configure rotary stages
+    azimuth_stage.configure(
+        angle_offset = ZABER_AZIMUTH_ANGLE_OFFSET,
+        limit_min = -90,
+        limit_max =  90,
+        max_speed = 30,
+        max_accel = 5
+    )
+
+    altitude_stage.configure(
+        angle_offset = ZABER_ALTITUDE_ANGLE_OFFSET,
+        limit_min = -90,
+        limit_max =  90,
+        max_speed = 30,
+        max_accel = 5
+    )
+
+    # Configure linear stages
+    x_stage.configure(
+        limit_min = 0,
+        limit_max = 50,
+        max_speed = 10,
+        max_accel = 10,
+    )
+
+    y_stage.configure(
+        limit_min = 0,
+        limit_max = 50,
+        max_speed = 10,
+        max_accel = 10,
+    )
+
+    z_stage.configure(
+        limit_min = 0,
+        limit_max = 50,
+        max_speed = 10,
+        max_accel = 10,
+    )
+
+    azimuth_stage.set_angle(20)
+    altitude_stage.set_angle(0)
+
+    x_stage.set_position(25.767)
+    y_stage.set_position(30.014)
+    z_stage.set_position(35.905)
